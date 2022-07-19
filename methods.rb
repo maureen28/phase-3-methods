7dd6d51c9e3ab9d516e35851279df74fd105c2d7
maureen <maureen.murimi@moringaschool.com>
2022-07-19 19:08:42 +0300

    commit tree b9d573fca7c379a7f3b63b6b7246d2f40f05aee4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,18 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+    "Hello, programmer!"
+end
+
+def greet(name = "Naureeen", name2 ="Jimmy")
+    puts "Hello, #{name}!"
+    puts "Hello, #{name2}!"
+end
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+def halve(integer)
+    return integer ? integer/2 : nil
+end
